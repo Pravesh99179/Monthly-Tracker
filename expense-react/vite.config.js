b8f6f40aee959cf6933ddd905db0d42236aef08b
Pravesh Agrawal <76154588+Pravesh99179@users.noreply.github.com>
2026-05-10 16:55:31 +0530

    Delete expense-react directory

--- expense-react/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/Monthly-Tracker/', // ← your GitHub repo name
-})
